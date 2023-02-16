3204c8ae8dd19c64737d2a23bc85acdd956e1fe0
elharitekamal <abdelkhalekelharite@gmail.com>
2023-02-16 09:56:57 +0100

    added

---
+++ blog/routes/web.php
@@ -0,0 +1,29 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+
+Route::get('/store', function () {
+    $filter = request('style');
+    if (isset($filter)) {
+        return 'this page is viewing <span style="color: red">' . $filter . '<span>';
+    } else {
+        return 'this page is viewing <span style="color: red">All Product<span>';
+
+    }
+});
